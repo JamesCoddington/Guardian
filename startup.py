b7ee86189992428a93c6eee2ba8370f1b79364e3
James Coddington <james@coddington.io>
2022-11-16 14:10:32 -0600

    Added intial codebase

--- startup.py
+++ startup.py
@@ -1,7 +1,15 @@
 #! var/bin/python3
+import pprint
+import socket
+import psutil
 
 def monitor():
-  print("Now Monitoring your nuts")
+    print("Now Monitoring your nuts")
+    # while(True):
+    for connection in psutil.net_connections():
+        print(connection)
+        # pprint(connection)
+
 
 def user_input():
   user = input("Would you like to start monitoring? [Y/n]\n")
