99329cf33291cf5aeabf40cebe2c0de71d73c33e
vietninh <vietquocninh@gmail.com>
2022-11-30 13:58:47 -0600

    Code fixes

--- test_startup.py
+++ test_startup.py
@@ -5,26 +5,22 @@ from startup import Guardian
 
 
 class TestStartup(unittest.TestCase):
+  guardian = Guardian()
 
   def test_user_input_yes(self):
-    guardian = Guardian()
-    guardian.get_input = MagicMock(return_value = "y")
-    guardian.monitor =  Mock()
-    guardian.user_input()
-    guardian.monitor.assert_called()
+    self.guardian.get_input = MagicMock(return_value = "y")
+    self.guardian.monitor =  Mock()
+    self.guardian.user_input()
+    self.guardian.monitor.assert_called()
 
   def test_user_input_invalid_response(self):
-    guardian = Guardian()
-    guardian.get_input = MagicMock(side_effect = ["NO", "n"])
-    self.assertEqual(guardian.user_input(), None)
+    self.guardian.get_input = MagicMock(side_effect = ["NO", "n"])
+    self.assertEqual(self.guardian.user_input(), None)
     
   def test_user_input_no(self):
-    guardian = Guardian()
-    guardian.get_input = MagicMock(return_value = "n")
-    self.assertEqual(guardian.user_input(), "Exiting the program")
+    self.guardian.get_input = MagicMock(return_value = "n")
+    self.assertEqual(self.guardian.user_input(), "Exiting the program")
   
-
-
 
 if __name__ == "__main__":
   unittest.main()
